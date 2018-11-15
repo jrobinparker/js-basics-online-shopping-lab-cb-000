f6da179239ab827b5f6ef613dc35695e3d446e2c
Jeremy Parker <jrobinparker@gmail.com>
2018-11-15 19:04:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cart = [];
+var removedItems = [];
 
 function getCart() {
  return cart;
@@ -10,21 +11,75 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * 100)
+  };
+  cart.push(newItem);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
-}
+  if ( getCart().length < 1 ) {
+    return 'Your shopping cart is empty.'
+  } else {
+    var itemsWithPrice = [];
+    getCart().forEach(function (item) {
+      itemsWithPrice.push(`${item.itemName} at $${item.itemPrice}`);
+    });
+      if ( itemsWithPrice.length === 1 ) {
+        return `In your cart, you have ${itemsWithPrice}.`;
+      } else if ( itemsWithPrice.length === 2 ) {
+        return `In your cart, you have ${itemsWithPrice.join(', and ')}.`;
+      } else if ( itemsWithPrice.length >= 3 ) {
+        return `In your cart, you have ${itemsWithPrice.slice(0, 2).join(', ') + ', and ' + itemsWithPrice.slice(2)}.`;
+      }
+    }
+  };
 
 function total() {
-  // write your code here
+  var prices = [];
+
+  cart.forEach(function (item) {
+    prices.push(item.itemPrice);
+  });
+
+  var sum = prices.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+
+  return sum
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(itemName) {
+  var itemToRemove = searchCartForItemToRemove(itemName)
+  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
 }
+
+function searchCartForItemToRemove(itemName) {
+  var searchResult
+  for (var i=0; i < getCart().length; i++) {
+    if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
+  }
+  return searchResult
+}
+
+function notifyUserThereIsNoItemToRemove() {
+  return 'That item is not in your cart.'
+}
+
+function removeItemFromCart(itemToRemove) {
+  var indexOfItemToRemove = cart.indexOf(itemToRemove)
+  getCart().splice(indexOfItemToRemove, 1)
+}
+
 
 function placeOrder(cardNumber) {
-  // write your code here
-}
+  if ( cardNumber === undefined ) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var sumToCharge = total();
+    setCart([])
+    return `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`;
+    }
+};
